5fd5690fb601d9d0c97004092790ff0af172cf94
charankrishna <charan.nithinj@gmail.com>
2018-08-04 22:13:12 +0530

    Match the string

---
+++ regexexe.js
@@ -0,0 +1,13 @@
+// Write a regex which matches "car" anywhere in the test string 
+
+var re = /car/gm;
+
+		var str1 = 'This is a car. I like my car.Cars are for transportation ';
+
+		console.log(re.test(str1));
+
+		var re = /learn/;
+
+		var str1 = ' ';
+
+		console.log(re.test(str1));
