72d9fd1cd54601ef3a5f907aa1cae34d0fac2637
charankrishna <charan.nithinj@gmail.com>
2018-08-04 22:25:19 +0530

    Match the string

--- regexexe.js
+++ regexexe.js
@@ -1,10 +1,22 @@
 // Write a regex which matches "car" anywhere in the test string 
 
-var re = /car/gm;
+var re = /\Dcar/gm;
 
 		var str1 = 'This is a car. I like my car.Cars are for transportation ';
 
 		console.log(re.test(str1));
+
+
+// Write a regex which matches 2011
+
+		var re = /\d2011/gm;
+
+		var str1 = 'I Visted Australia in 2011 ';
+
+		console.log(re.test(str1));
+
+//  
+
 
 		var re = /learn/;
 
