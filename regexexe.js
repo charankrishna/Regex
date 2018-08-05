958c2f5ba16fcbe57491fb6c686a1cd4b72b4823
charankrishna <charan.nithinj@gmail.com>
2018-08-05 23:09:50 +0530

    Match the string

--- regexexe.js
+++ regexexe.js
@@ -293,3 +293,12 @@ var re = /\Dcar/gm;
 		var str1 = '123alfa '
 
 		console.log(re.test(str1));
+
+
+// 	Write a regex to match flavor and flavour
+
+		var	re = /\bflavou?r\b/g;
+
+		var str1 = 'flavor flavour'
+
+		console.log(re.test(str1));
